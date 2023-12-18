26a8282337b21b6126dd6b715cd41c245bbfc66d
Phutana Pratumton <154107511+Pae-Phutana@users.noreply.github.com>
2023-12-18 11:07:24 +0700

    เพิ่มไฟล์ week1

---
+++ week1/index.php
@@ -0,0 +1,13 @@
+<?php
+    $address = "moo.4 singcapo";
+    $char1 = 'A';
+    echo $address[4].$address[3].$address[0].$address[1].$address[2];
+    echo "<br>";
+    echo $char1;
+    $age = 17;
+    $p1 = 3.14;
+    echo $age+$p1;
+    echo "<br>";
+    $isTrue = false;
+    echo $isTrue;
+?>
